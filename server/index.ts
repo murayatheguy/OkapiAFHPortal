1d747205039749ece9c034952354d23e1e46fbee
Francis Wanja <francis.wanja@gmail.com>
2025-12-22 23:22:24 -0800

    Fix cookie sameSite to lax for better compatibility

--- server/index.ts
+++ server/index.ts
@@ -43,7 +43,7 @@ app.use(
       secure: process.env.NODE_ENV === "production",
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
-      sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
+      sameSite: 'lax',
     },
   })
 );
